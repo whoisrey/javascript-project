6e8e388bad7ede73d6a725cb6c30c0e05e78dd13
holaleche <whoisleche@gmail.com>
2023-09-12 11:20:31 +0900

    carousel

---
+++ carousel/script.js
@@ -0,0 +1,41 @@
+const arrPic = [
+  "https://raw.githubusercontent.com/nugurejeil/sharingPhoto/main/jammanbo.png",
+  "https://raw.githubusercontent.com/nugurejeil/sharingPhoto/main/ev.png?raw=true",
+  "https://raw.githubusercontent.com/nugurejeil/sharingPhoto/main/mazayoung.png?raw=true",
+  "https://raw.githubusercontent.com/nugurejeil/sharingPhoto/main/mobugi.png",
+  "https://raw.githubusercontent.com/nugurejeil/sharingPhoto/main/nyaong.png",
+  "https://raw.githubusercontent.com/nugurejeil/sharingPhoto/main/pulin.png",
+  "https://raw.githubusercontent.com/nugurejeil/sharingPhoto/main/weirdseed.png",
+];
+const center = document.querySelector(".list-item");
+
+arrPic.forEach((item) => {
+  const elLi = document.createElement("li");
+  elLi.innerHTML = `<img src="${item}" alt="">`;
+  center.appendChild(elLi);
+});
+
+const items = document.querySelectorAll(".list-item li");
+
+const radius = (items[0].offsetWidth * items.length) / 3.14 / 2;
+
+items.forEach((item, index) => {
+  item.style.transform = `rotateY(${
+    (360 / items.length) * index
+  }deg) translateZ(${radius}px)`;
+});
+
+const angle = 360 / items.length;
+let currAngle = 0;
+
+document.addEventListener("click", function (event) {
+  // 화면 오른쪽을 눌렀을 경우
+  if (window.innerWidth / 2 < event.clientX) {
+    currAngle += angle;
+
+    // 화면 왼쪽을 눌렀을 경우
+  } else {
+    currAngle -= angle;
+  }
+  center.style.transform = `translate(-50%, -50%) rotateY(${currAngle}deg)`;
+});
