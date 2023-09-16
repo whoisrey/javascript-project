803a1b835494f9ecc726171e3d4d4cf86a3a8b63
holaleche <whoisleche@gmail.com>
2023-09-16 20:12:44 +0900

    card-layout

--- card-layout/js/firstcard.js
+++ card-layout/js/firstcard.js
@@ -1,13 +1,15 @@
+const modal = document.querySelector(".modal");
 const firstSection = document.querySelector(".first-section");
 const firstCard = document.querySelector(".first-card");
 
 firstCard.addEventListener("click", function () {
-  firstSection.style.background = "rgba(0, 0, 0, 0.7)";
+  modal.style.display = "block";
 });
 
 // 모달 외부를 클릭하면 배경색 원래 상태로 변경
 window.addEventListener("click", function (event) {
-  if (event.target === firstSection) {
-    firstSection.style.background = "seashell";
+  if (event.target === modal) {
+    // firstSection.style.background = "seashell";
+    modal.style.display = "none";
   }
 });
