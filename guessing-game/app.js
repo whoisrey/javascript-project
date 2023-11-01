46568fdf2b3bae3e29e34e0edc30e1469a3254ec
holaleche <whoisleche@gmail.com>
2023-11-01 19:05:16 +0900

    guessing-game

---
+++ guessing-game/app.js
@@ -0,0 +1,34 @@
+let maximum = parseInt(prompt("최댓값을 입력하시오."));
+while (!maximum) {
+  maximum = parseInt(prompt("유효한 숫자를 입력하시오."));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = prompt(
+  "정답을 추측하여 입력하시오. 'q'를 입력하면 포기할 수 있습니다."
+);
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+  if (guess.toLowerCase() === "q") break;
+  guess = parseInt(guess);
+  if (guess > targetNum) {
+    guess = prompt("숫자가 커요");
+    attempts++;
+  } else if (guess < targetNum) {
+    guess = prompt("숫자가 작아요");
+    attempts++;
+  } else {
+    guess = prompt(
+      "유효한 숫자를 입력하시오. 'q'를 입력하면 포기할 수 있습니다."
+    );
+  }
+}
+
+if (guess.toLowerCase() === "q") {
+  console.log("안녕히 가세요.");
+} else {
+  console.log(`${attempts}번째 만에 정답입니다.`);
+}
