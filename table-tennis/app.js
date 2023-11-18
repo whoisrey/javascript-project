3cbebbff54ddbb3d5268985c7a73069134432a2b
holaleche <whoisleche@gmail.com>
2023-11-18 18:11:48 +0900

    table-tennis

---
+++ table-tennis/app.js
@@ -0,0 +1,52 @@
+const p1Score = document.getElementById("leftScore");
+const p2Score = document.getElementById("rightScore");
+const p1Button = document.getElementById("leftBtn");
+const p2Button = document.getElementById("rightBtn");
+const resetButton = document.getElementById("resetBtn");
+const matchPoint = document.getElementById("matchPoint");
+
+let isGameOver = false;
+let winningScore = 3;
+
+matchPoint.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  reset();
+});
+
+let p1Point = 0;
+p1Button.addEventListener("click", () => {
+  if (!isGameOver) {
+    p1Point += 1;
+    p1Score.textContent = `${p1Point}`;
+    if (p1Point === winningScore) {
+      isGameOver = true;
+      p1Score.classList.add("winner");
+      p2Score.classList.add("loser");
+    }
+  }
+});
+
+let p2Point = 0;
+p2Button.addEventListener("click", () => {
+  if (!isGameOver) {
+    p2Point += 1;
+    p2Score.textContent = `${p2Point}`;
+    if (p2Point === winningScore) {
+      isGameOver = true;
+      p1Score.classList.add("loser");
+      p2Score.classList.add("winner");
+    }
+  }
+});
+
+const reset = () => {
+  isGameOver = false;
+  p1Point = 0;
+  p2Point = 0;
+  p1Score.textContent = 0;
+  p2Score.textContent = 0;
+  p1Score.classList.remove("winner", "loser");
+  p2Score.classList.remove("winner", "loser");
+};
+
+resetButton.addEventListener("click", reset);
