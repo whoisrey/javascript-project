a50b9a6eb409b63ce1918e5a1250fc5b5451a712
holaleche <whoisleche@gmail.com>
2024-05-12 12:38:43 +0900

    javascript-project

--- behavior-delegation/index.js
+++ behavior-delegation/index.js
@@ -1,3 +1,5 @@
+// OO (aka. CO) 객체 지향, 클래스 지향 디자인 패턴
+
 function Vehicle(name) {
   this.name = name;
 }
@@ -24,6 +26,7 @@ c2.run(); // "안녕하세요 기아차 시동걸까요?"
 
 ////////////////->
 
+// OLOO Objects Linked to Other Objects
 const Vehicle = {
   init: function (name) {
     this.name = name;
