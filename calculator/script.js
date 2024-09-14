b24b3a99d1d528649676ff482a4ea3b4f77615d5
holaleche <whoisleche@gmail.com>
2024-09-14 22:39:32 +0900

    javascript-project

--- calculator/script.js
+++ calculator/script.js
@@ -1,9 +1,9 @@
 let runningTotal = 0;
-let buffer = '0';
+let buffer = "0";
 let previousOperator;
 let shouldResetBuffer = false;
 
-const screen = document.querySelector('.screen');
+const screen = document.querySelector(".screen");
 
 function buttonClick(value) {
   if (isNaN(value)) {
@@ -16,11 +16,11 @@ function buttonClick(value) {
 
 function handleSymbol(symbol) {
   switch (symbol) {
-    case 'C':
-      buffer = '0';
+    case "C":
+      buffer = "0";
       runningTotal = 0;
       break;
-    case '=':
+    case "=":
       if (previousOperator === null) {
         return;
       }
@@ -29,24 +29,24 @@ function handleSymbol(symbol) {
       buffer = runningTotal.toString();
       runningTotal = 0;
       break;
-    case '←':
+    case "←":
       if (buffer.length === 1) {
-        buffer = '0';
+        buffer = "0";
       } else {
         buffer = buffer.substring(0, buffer.length - 1);
       }
       break;
-    case '+':
-    case '−':
-    case '×':
-    case '÷':
+    case "+":
+    case "−":
+    case "×":
+    case "÷":
       handleMath(symbol);
       break;
   }
 }
 
 function handleMath(symbol) {
-  if (buffer === '0') {
+  if (buffer === "0") {
     return;
   }
 
@@ -62,19 +62,19 @@ function handleMath(symbol) {
 }
 
 function flushOperation(intBuffer) {
-  if (previousOperator === '+') {
+  if (previousOperator === "+") {
     runningTotal += intBuffer;
-  } else if (previousOperator === '−') {
+  } else if (previousOperator === "−") {
     runningTotal -= intBuffer;
-  } else if (previousOperator === '×') {
+  } else if (previousOperator === "×") {
     runningTotal *= intBuffer;
-  } else if (previousOperator === '÷') {
+  } else if (previousOperator === "÷") {
     runningTotal /= intBuffer;
   }
 }
 
 function handleNumber(numberString) {
-  if (buffer === '0' || shouldResetBuffer) {
+  if (buffer === "0" || shouldResetBuffer) {
     buffer = numberString;
     shouldResetBuffer = false;
   } else {
@@ -84,8 +84,8 @@ function handleNumber(numberString) {
 
 function init() {
   document
-    .querySelector('.calc-buttons')
-    .addEventListener('click', function (event) {
+    .querySelector(".calc-buttons")
+    .addEventListener("click", function (event) {
       buttonClick(event.target.innerText);
     });
 }
