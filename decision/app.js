17cc273cb1ea94833a5458e3714b3ac4c06af733
holaleche <whoisleche@gmail.com>
2024-09-17 22:48:57 +0900

    javascript-project

--- decision/app.js
+++ decision/app.js
@@ -16,12 +16,9 @@ const form = document.querySelector(".img-form");
 const amount = document.getElementById("amount");
 const result = document.querySelector(".img-container");
 
-// submit 이벤트리스너 등록
 form.addEventListener("submit", function (e) {
-  // 기본 동작 취소
   e.preventDefault();
 
-  // 선택된 이미지들을 HTML 요소로 변환하여 src에 추가 (result 형태로)
   const value = parseInt(amount.value);
   let selectedImages = [];
   if (value >= 1 && value <= 3) {
@@ -36,7 +33,6 @@ form.addEventListener("submit", function (e) {
   imageContainer.style.marginBottom = "10px";
 });
 
-// 주어진 개수(1~3개)만큼 임의의 이미지를 선택하여 배열로 반환
 function getRandomImages(count) {
   const randomImages = [];
   for (let i = 0; i < count; i++) {
