cb3398a2967762fe16ca7a8639caac9d204c0f90
holaleche <whoisleche@gmail.com>
2024-09-28 22:57:31 +0900

    javascript-project

--- color-flipper/js/hex.js
+++ color-flipper/js/hex.js
@@ -1,5 +1,5 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-// # f15025
+
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
@@ -16,20 +16,3 @@ btn.addEventListener("click", function () {
 function getRandomNumber() {
   return Math.floor(Math.random() * hex.length);
 }
-
-// DOM
-// document.getElementById
-// document.querySelector
-
-// Node
-// .textContent
-// .innerHTML
-
-// for loop
-
-// Math Method
-// Math.random 0 ~ 1  부터의 난수
-// Math.floor
-// Math.ceil // undefined
-
-// function
