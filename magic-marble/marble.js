d51f423da19b932d5bbf8a16795e7428d09b62a1
holaleche <whoisleche@gmail.com>
2025-01-28 20:31:47 +0900

    javascript-project

--- magic-marble/marble.js
+++ magic-marble/marble.js
@@ -2,8 +2,8 @@ const inpPicker = document.querySelectorAll(".inp-picker");
 inpPicker.forEach((item) => {
   item.addEventListener("input", () => {
     const name = "--main-" + item.dataset.id;
-    // const name = `--main-${item.dataset.id}`;
     const root = document.querySelector(":root");
+
     root.style.setProperty(name, item.value);
   });
 });
