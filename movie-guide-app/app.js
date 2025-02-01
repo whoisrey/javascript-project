ee49da49d30ee8cde1257415329bba93bdd8df03
holaleche <whoisleche@gmail.com>
2025-02-01 21:26:46 +0900

    javascript-project

--- movie-guide-app/app.js
+++ movie-guide-app/app.js
@@ -2,19 +2,16 @@ let movieNameRef = document.getElementById("movie-name");
 let searchBtn = document.getElementById("search-btn");
 let result = document.getElementById("result");
 
-//function to fetch data from API
 let getMovie = () => {
   let movieName = movieNameRef.value;
   let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
 
-  //if input field is empty
   if (movieName.length <= 0) {
     result.innerHTML = `<h3 class="msg">Please enter a movie name</h3>`;
   } else {
     fetch(url)
       .then((resp) => resp.json())
       .then((data) => {
-        //if movie exists in database
         if (data.Response === "True") {
           result.innerHTML = `
             <div class="info">
