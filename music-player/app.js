24ec96c223a0bc20dc0b00c0c99f8dc7b39002c2
holaleche <whoisleche@gmail.com>
2025-02-03 10:43:59 +0900

    javascript-project

--- music-player/app.js
+++ music-player/app.js
@@ -1,14 +1,14 @@
-const image = document.getElementById("cover"),
-  title = document.getElementById("music-title"),
-  artist = document.getElementById("music-artist"),
-  currentTimeEl = document.getElementById("current-time"),
-  durationEl = document.getElementById("duration"),
-  progress = document.getElementById("progress"),
-  playerProgress = document.getElementById("player-progress"),
-  prevBtn = document.getElementById("prev"),
-  nextBtn = document.getElementById("next"),
-  playBtn = document.getElementById("play"),
-  background = document.getElementById("bg-img");
+const image = document.getElementById("cover");
+const title = document.getElementById("music-title");
+const artist = document.getElementById("music-artist");
+const currentTimeEl = document.getElementById("current-time");
+const durationEl = document.getElementById("duration");
+const progress = document.getElementById("progress");
+const playerProgress = document.getElementById("player-progress");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const playBtn = document.getElementById("play");
+const background = document.getElementById("bg-img");
 
 const music = new Audio();
 
