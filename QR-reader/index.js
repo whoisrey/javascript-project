601e553d863e16121a41389420e28700e8bd0c32
holaleche <whoisleche@gmail.com>
2025-02-16 11:47:47 +0900

    javascript-project

--- QR-reader/index.js
+++ QR-reader/index.js
@@ -5,7 +5,6 @@ const infoText = document.querySelector("p");
 const closeBtn = document.querySelector(".close");
 const copyBtn = document.querySelector(".copy");
 
-// API로 부터 Data를 fetch받는 함수
 function fetchRequest(file, formData) {
   infoText.innerText = "Scanning QR Code...";
   fetch("http://api.qrserver.com/v1/read-qr-code/", {
@@ -28,7 +27,6 @@ function fetchRequest(file, formData) {
     });
 }
 
-// 업로드한 QR Code File을 API에 Request 요청을 진행하는 이벤트
 fileInp.addEventListener("change", async (e) => {
   let file = e.target.files[0];
   if (!file) return;
@@ -37,16 +35,13 @@ fileInp.addEventListener("change", async (e) => {
   fetchRequest(file, formData);
 });
 
-// 클립보드에 텍스트를 복사
 copyBtn.addEventListener("click", () => {
   let text = document.querySelector("textarea").textContent;
   navigator.clipboard.writeText(text);
 });
 
-// form 요소를 클릭하면 파일이 업로드를 실행하는 이벤트
 form.addEventListener("click", () => fileInp.click());
 
-// 이미지를 드래그해서 업로드하는 기능 추가
 form.addEventListener("dragover", (e) => {
   e.preventDefault();
   document.body.classList.add("active");
@@ -68,14 +63,6 @@ form.addEventListener("drop", (e) => {
   fetchRequest(file, formData);
 });
 
-// closeBtn.addEventListener("click", () => wrapper.classList.remove("active"));
-
-// closeBtn.addEventListener("click", (e) => {
-//   e.stopPropagation();
-//   wrapper.classList.remove("active");
-// });
-
-// close 버튼을 누르고 다시 업로드할 수 있는 기능을 구현?
 closeBtn.addEventListener("click", (e) => {
   e.stopPropagation();
   wrapper.classList.remove("active");
