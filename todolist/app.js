a7524c326b3e1d18fad168cc6893d14990e395a0
holaleche <whoisleche@gmail.com>
2025-02-25 06:24:50 +0900

    javascript-project

--- todolist/app.js
+++ todolist/app.js
@@ -13,8 +13,8 @@ while (input !== "quit" && input !== "q") {
     todo.push(add);
     console.log(`${add}를 추가했습니다.`);
   } else if (input === "delete") {
-    const index = parseInt(prompt("삭제할 일의 번호를 입력하시오."));
-    if (!Number.isNaN(index) && index <= parseInt(todo.length - 1)) {
+    const index = Number.parseInt(prompt("삭제할 일의 번호를 입력하시오."));
+    if (!Number.isNaN(index) && index <= Number.parseInt(todo.length - 1)) {
       const deleted = todo.splice(index, 1);
       console.log(`${index}번째 할 일을 삭제했습니다.`);
     } else {
@@ -25,31 +25,3 @@ while (input !== "quit" && input !== "q") {
 }
 
 console.log("프로그램을 종료하겠습니다.");
-
-// let input = prompt("what would you like to do?");
-// const array = [];
-
-// while (input !== "quit" && input !== "q") {
-//   input = prompt("what would you like to do?");
-//   if (input === "new") {
-//     input = prompt("Enter new todo.");
-//     array.push(input);
-//     console.log(`${input} added to do list.`);
-//   } else if (input === "delete") {
-//     const index = parseInt(prompt("Enter index of todo to delete."));
-//     if (index <= array.length) {
-//       const deleted = array.splice(input, 1);
-//       console.log(`${deleted} removed.`);
-//     } else {
-//       console.log("Do it again..");
-//     }
-//   } else if (input === "list") {
-//     console.log("************");
-//     for (let i = 0; i < array.length; i++) {
-//       console.log(`${i} : ${array[i]}`);
-//     }
-//     console.log("************");
-//   }
-// }
-
-// console.log("OK, YOU QUIT THIS APP.");
